667f1f3e4c1926b4e69e7a5831f768d68478a43f
VDC\tnguyen482 <tnguyen482@csc.com>
2018-10-26 18:07:30 +0700

    Upload new project

---
+++ WebApplication1/DAL/MvcMovieContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.DAL
+{
+    public class MvcMovieContext : DbContext
+    {
+        public MvcMovieContext(DbContextOptions<MvcMovieContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Models.Movie> Movie { get; set; }
+    }
+}
